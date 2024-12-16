2bd07412bd295523b927b9b57c7717f9542c3a4f
Prasanth <prasanth76200@gmail.com>
2024-12-16 11:49:22 +0530

    Port

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const config = require('./config/config');
 
 // const Port = process.env.X_ZOHO_CATALYST_LISTEN_PORT || 3000;
-app.listen(3000, () => {
+app.listen(process.env.X_ZOHO_CATALYST_LISTEN_PORT || 9000, () => {
   
   console.log("Server Started")
 })
