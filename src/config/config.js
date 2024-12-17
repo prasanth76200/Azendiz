5c1179145d97276ed864733da489343ea5c148e1
Prasanth <prasanth76200@gmail.com>
2024-12-17 16:38:16 +0530

    Changes

--- src/config/config.js
+++ src/config/config.js
@@ -8,24 +8,24 @@ dotEnv.config();
 module.exports = {
   db: {
 
-    host:'localhost', // Use environment variable if available
+    // host:'localhost', // Use environment variable if available
+    // user: 'root',
+    // password:  '',
+    // database: 'azendiz',
+    // port: 3306,
+    // connectionLimit: 100, // Max number of connections in the pool
+    // waitForConnections: true,
+    // queueLimit: 0,
+    // connectTimeout: 604800, // Timeout for initial connection
+    host:'103.7.11.29', // Use environment variable if available
     user: 'root',
-    password:  '',
+    password:  '@Zend1z@24!@#',
     database: 'azendiz',
     port: 3306,
     connectionLimit: 100, // Max number of connections in the pool
     waitForConnections: true,
     queueLimit: 0,
     connectTimeout: 604800, // Timeout for initial connection
-    // host:'103.7.11.29', // Use environment variable if available
-    // user: 'root',
-    // password:  '@Zend1z@24!@#',
-    // database: 'azendiz',
-    // port: 3306,
-    // connectionLimit: 10, // Max number of connections in the pool
-    // waitForConnections: true,
-    // queueLimit: 0,
-    // connectTimeout: 604800, // Timeout for initial connection
   },
   port: '3000',
 };
